96ed21d9fb2bc53f32f8c8cd464b384ea21d8646
Oriana Bautista <oribautista99@gmail.com>
2023-03-16 20:56:08 -0400

    Estructura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { AppShell, Header, Navbar, Text } from "@mantine/core";
 import React from "react";
+import HeaderSimple from "./components/Header/Header";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <AppShell
+      padding="md"
+      navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{
+        <Text>Navbar</Text>
+        }</Navbar>}
+      header={<Header height={60} p="xs">{
+        <HeaderSimple></HeaderSimple>
+        }</Header>}
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+    >
+      {
+        <Text>Pages</Text>
+      }
+    </AppShell>
+  );
 };
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,61 @@
+import { createStyles, Header, Container, Group, rem, Button } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+  },
+
+  links: {
+    [theme.fn.smallerThan('xs')]: {
+      display: 'none',
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan('xs')]: {
+      display: 'none',
+    },
+  },
+
+  link: {
+    display: 'block',
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: 'none',
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    },
+  },
+
+  linkActive: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+    },
+  },
+}));
+
+function HeaderSimple() {
+  const { classes } = useStyles();
+
+  return (
+    <Header height={60} mb={120}>
+      <Container className={classes.header}>
+        <Group spacing={5}  className={classes.hiddenMobile}>
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+        </Group>
+      </Container>
+    </Header>
+  );
+}
+
+export default HeaderSimple;
